1193ffc2f2959c2cf3148ec313f397556d9e64d8
Alise <arponenalise@gmail.com>
2024-08-03 11:57:54 +0200

    separated color put from the rendered function

--- src/draw/draw.c
+++ src/draw/draw.c
@@ -6,7 +6,7 @@
 /*   By: aarponen <aarponen@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/27 13:09:43 by aarponen          #+#    #+#             */
-/*   Updated: 2024/08/03 11:13:48 by aarponen         ###   ########.fr       */
+/*   Updated: 2024/08/03 11:56:47 by aarponen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,12 @@ t_vector	ft_calculate_pixel_center(t_camera *camera, int x, int y)
 	return (pixel_center);
 }
 
-// Calculate the color of the pixel based on co
+// Calculate the color and put it to image:
+// just to test out, calculate color based on the xyz hitpoint
+void	ft_put_color(t_data *data, int x, int y, t_hit hit)
+{
+	ft_my_mlx_pixel_put(data->mlx, x, y, ft_trgb(1, hit.sphere->color));
+}
 
 // Draw the scene:
 // Iterate through the screen pixels and calculate the color for each pixel.
@@ -56,7 +61,7 @@ void	ft_draw_scene(t_data *data)
 			if (data->sphere)
 				hit_result = ft_hit_sphere(data, ray);
 			if (hit_result.distance < INFINITY)
-				ft_my_mlx_pixel_put(data->mlx, x, y, ft_trgb(1, hit_result.sphere->color));
+				ft_put_color(data, x, y, hit_result);
 			x++;
 		}
 		y++;
